95484ba71e077f64ed92b237133c861c7f7db7c1
Muhammad Jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2022-07-04 16:26:03 +0700

    change db server

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "sql6.freemysqlhosting.net",
-  USER: "sql6502107",
-  PASSWORD: "F2K2kdEeuX",
-  DB: "sql6502107"
+  HOST: "sql8.freemysqlhosting.net",
+  USER: "sql8503908",
+  PASSWORD: "zgAtbQe9jW",
+  DB: "sql8503908"
 };
